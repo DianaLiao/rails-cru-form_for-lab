18333f9e69b01dfabe6bc4bcc43843dc7d0f53e4
Diana Liao <dijali@gmail.com>
2021-03-16 20:45:03 -0400

    Add songs, artists, genres models, controllers, basic routes and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+Artist.reset_pk_sequence
+Genre.reset_pk_sequence
+Song.reset_pk_sequence
+
+range = (1..10).to_a
+
+10.times do
+  Artist.create(name:Faker::Music.band, bio:Faker::Lorem.paragraph)
+  Genre.create(name:Faker::Music.genre)
+  Song.create(name:Faker::Book.title, artist_id:range.sample, genre_id:range.sample)
+end
+
+puts "db loaded up!"
